c7852c310b2814a16bb8f7476cda8ce8b6440e37
Your Name <barcellinisilvia@gmail.com>
2021-07-16 16:59:58 +0200

    ✏️ fix typo

--- calculator.js
+++ calculator.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express(); 
-app.use(bodyParser.urlencoded({extended = true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res){
     res.sendFile(__dirname + "/index.html");
 }); 
 
 app.post("/", function (req, res){
+    console.log(req.body)
     res.send("thanks for posting!");
 }); 
 
